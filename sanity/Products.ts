74abb9d7a61172aa292b7e4b43c67b152b7766c0
Adil sheikh <sheikhadil044@gmail.com>
2023-08-19 14:40:53 -0700

    new work depoly

---
+++ sanity/Products.ts
@@ -0,0 +1,61 @@
+import { defineField, defineType } from "sanity"
+
+export default defineType ({
+    name: "products",
+    type: "document",
+    title: "products",
+
+    fields:[
+       
+        
+        defineField({
+
+            name : "image",
+            type : "image",
+            title : "Image",
+        }),
+        defineField ({
+
+            name : "title",
+           type : "string",
+            title : "Title",
+        }),
+        defineField({
+
+            name : "slug",
+            type : "slug",
+            title : "slug",
+            options:{
+               source : "title",
+               maxLength : 50,
+            }
+        }),
+        defineField({
+
+            name : "decription",
+            type : "string",
+            title : "Description",
+        }),
+        defineField({
+
+            name : "price",
+            type : "number",
+            title : "price",
+        }),
+        defineField({
+
+            name : "category",
+            type : "string",
+            title : "category",
+            options:{
+                list:[
+                    {title: "Man", value:"man"},
+                    {title: "Women", value:"women"},
+                    {title: "Kids", value:"kids"},
+
+                ]
+            }
+        }),
+
+    ],
+});
