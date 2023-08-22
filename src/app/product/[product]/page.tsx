61c6426812b065238abc9b6074c52fbcae297785
Adil sheikh <sheikhadil044@gmail.com>
2023-08-22 11:52:44 -0700

    dp

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import FetchData from '../../../../sanity/FetchData';
 
-function page() {
+export default  async function  page({params}: {params:any}) {
+
+  const data = await FetchData();
+  //console.log("data",data);
+  const filteredData = data.find((item : any ) => item.slug.current == params.product);
+
+  //console.log("params", filteredData );
   return (
-    <div> hello page</div>
+    <div>
+      <h1>Title:{filteredData.title}</h1>
+      <h1>description:{filteredData.description}</h1>
+      <h1>category:{filteredData.category}</h1>
+      <h1>price:{filteredData.price}</h1>
+</div>
   )
 }
-
-export default page
