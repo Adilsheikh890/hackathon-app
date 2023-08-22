18e1602e69a2174a7dee1bc3dc9902496f96ec1e
Adil sheikh <sheikhadil044@gmail.com>
2023-08-22 12:38:38 -0700

    iteam

--- src/app/Productx/page.tsx
+++ src/app/Productx/page.tsx
@@ -13,8 +13,8 @@ export default async function page () {
   
   return (
     <div className="flex md:flex-row flex-col gap-2 items-center text-black md:mt-20 rounded-full">
-       {data.map((product :any , index : number)=>(
-          <Link href ={`/product/${product.slug.current}`} className='p-5' key={product}>
+       {data.map((product :any , iteam : number)=>(
+          <Link href ={`/product/${product.slug.current}`} className='p-5' key={iteam}>
           
           <Image src={urlForImage(product.image).url()} alt=''  width={300} height={300} className=" w-full h-80 object-cover  object-center"/>
             <h1>{product.title}</h1>
