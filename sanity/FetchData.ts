b45557eaa76107b13560c19855ca5b6c8f6977a5
Adil sheikh <sheikhadil044@gmail.com>
2023-08-22 11:49:23 -0700

    new changes

--- sanity/FetchData.ts
+++ sanity/FetchData.ts
@@ -2,6 +2,6 @@ import React from 'react'
 import { client } from './lib/client'
 
 export default  async function FetchData() {
-    const  data =  await client.fetch (`*[_type =="products"]`) 
+    const  data =  await client.fetch(`*[_type =="products"]`) 
   return data
 }
