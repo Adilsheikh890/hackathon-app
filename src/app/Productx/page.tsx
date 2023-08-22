a9c4d8bcb49efcfef5c2a4b4dcce103c21570a6b
Adil sheikh <sheikhadil044@gmail.com>
2023-08-22 12:17:54 -0700

    cg

--- src/app/Productx/page.tsx
+++ src/app/Productx/page.tsx
@@ -13,8 +13,8 @@ export default async function page () {
   
   return (
     <div className="flex md:flex-row flex-col gap-2   text-black md:mt-20 rounded-full">
-       {data.map((product :any , i : number)=>(
-          <Link href ={`/product/${product.slug.current}`} className='p-5'key={i}>
+       {data.map((product :any , id : number)=>(
+          <Link href ={`/product/${product.slug.current}`} className='p-5' key={product.id}>
           
           <Image src={urlForImage(product.image).url()} alt=''  width={300} height={300} className=" w-full h-80 object-cover  object-center"/>
             <h1>{product.title}</h1>
