8785e831348a024e32fdc0450f291e4f679b50f7
Adil sheikh <sheikhadil044@gmail.com>
2023-08-26 14:44:38 -0700

    new

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,10 +2,10 @@
 import React from 'react'
 import CartItems from "../Componentx/CartItems"
 
-export default function page(items : number) {
+export default function page() {
   return (
     
-    <CartItems key={items}/>
+    <CartItems />
 
   )
 }
