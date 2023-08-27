45276919a9f88de917cb0051bcd641fdb9a5b529
Adil sheikh <sheikhadil044@gmail.com>
2023-08-27 11:53:33 -0700

    new

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import FetchData from '../../../../sanity/FetchData';
-import { Button } from '@/components/ui/button';
+//import { Button } from '@/components/ui/button';
 import  ProductsDetails from '../../Componentx/ProductsDetails';
 
 
